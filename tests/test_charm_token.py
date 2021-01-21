50a65c76c927f28309aa1f0704d06d08ed421ff8
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-01-21 13:54:47 +0000

    Rewrite charm token so that it has a hard cap

--- tests/test_charm_token.py
+++ tests/test_charm_token.py
@@ -6,35 +6,6 @@ def test_charm_token(CharmToken, accounts):
     charm = deployer.deploy(CharmToken)
     assert charm.name() == "Charm"
     assert charm.symbol() == "CHARM"
-    assert charm.governance() == deployer
-
-    # check permissions
-    with reverts("!governance"):
-        charm.setGovernance(user, {"from": user})
-
-    with reverts("!minter"):
-        charm.mint(user, 1e18, {"from": user})
-
-    with reverts("!governance"):
-        charm.addMinter(user, {"from": user})
-
-    with reverts("!governance"):
-        charm.removeMinter(user, {"from": user})
-
-    # add user as minter and mint 1
-    charm.addMinter(user, {"from": deployer})
-    assert charm.balanceOf(user) == 0
-    charm.mint(user, 1e18, {"from": user})
-    assert charm.balanceOf(user) == 1e18
-
-    with reverts("!minter"):
-        charm.mint(user, 1e18, {"from": deployer})
-
-    # remove user as minter and check they can't mint any more
-    charm.removeMinter(user, {"from": deployer})
-    with reverts("!minter"):
-        charm.mint(user, 1e18, {"from": user})
-
-    # set user as governance
-    charm.setGovernance(user, {"from": deployer})
-    assert charm.governance() == user
+    assert charm.decimals() == 18
+    assert charm.totalSupply() == 1e8 * 1e18
+    assert charm.balanceOf(deployer) == 1e8 * 1e18
