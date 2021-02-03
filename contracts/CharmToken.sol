da2dc9320b773702f09fb54f5e4d39044f784b52
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-02-03 12:12:12 +0000

    Mint initial tokens to treasury instead of sender

--- contracts/CharmToken.sol
+++ contracts/CharmToken.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.6.12;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CharmToken is ERC20 {
-    constructor() public ERC20("Charm", "CHARM") {
+    constructor(address treasury) public ERC20("Charm", "CHARM") {
         // total supply is 100 million
-        _mint(msg.sender, 1e8 * 1e18);
+        _mint(treasury, 1e8 * 1e18);
     }
 }
--- tests/test_charm_token.py
+++ tests/test_charm_token.py
@@ -2,10 +2,9 @@ from brownie import reverts
 
 
 def test_charm_token(CharmToken, accounts):
-    deployer, user = accounts[:2]
-    charm = deployer.deploy(CharmToken)
+    deployer, treasury = accounts[:2]
+    charm = deployer.deploy(CharmToken, treasury)
     assert charm.name() == "Charm"
     assert charm.symbol() == "CHARM"
     assert charm.decimals() == 18
-    assert charm.totalSupply() == 1e8 * 1e18
-    assert charm.balanceOf(deployer) == 1e8 * 1e18
+    assert charm.totalSupply() == charm.balanceOf(treasury) == 1e8 * 1e18
